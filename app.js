fb3cbc1ed6ffbf6eeeb59d5b6f9e8d5bfc736009
Mitchell Tyrer <mltcinematography@gmail.com>
2018-12-24 18:28:54 +0000

    still working

--- app.js
+++ app.js
@@ -30,9 +30,22 @@ app.get("/blogs", function(req, res){
         } else {
             res.render("index", {blogs: blogs});
         }
-    })
-})
-    
+    });
+});
+
+app.get("/blogs/new", function(req, res) {
+   res.render("new"); 
+});
+
+app.post("/blogs", function(req, res){
+    Blog.create(req.body.blog, function(err, newBlog){
+        if(err){
+            res.render("new");
+        } else {
+            res.redirect("/blogs");
+        }
+    });
+});
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("server is up");
